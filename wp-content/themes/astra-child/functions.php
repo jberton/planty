db29cb80494d076adfdc9e6693fd098d20de4bd3
Jeremie Berton <bertonjer@hotmail.com>
2024-03-12 23:11:25 +0100

    Ajout des fichiers RAF hook filter

--- wp-content/themes/astra-child/functions.php
+++ wp-content/themes/astra-child/functions.php
@@ -1,25 +1,34 @@
 <?php
-/**
- * Astra Child Theme functions and definitions
- *
- * @link https://developer.wordpress.org/themes/basics/theme-functions/
- *
- * @package Astra Child
- * @since 1.0.0
- */
-
-/**
- * Define Constants
- */
-define( 'CHILD_THEME_ASTRA_CHILD_VERSION', '1.0.0' );
-
-/**
- * Enqueue styles
- */
-function child_enqueue_styles() {
-
-	wp_enqueue_style( 'astra-child-theme-css', get_stylesheet_directory_uri() . '/style.css', array('astra-theme-css'), CHILD_THEME_ASTRA_CHILD_VERSION, 'all' );
-
+add_action('wp_enqueue_scripts', 'theme_enqueue_styles');
+// création d'une fonction pour appeler le style.css du thème parent
+// get_template_directory_uri() renvoi l'URL du thème parent
+function theme_enqueue_styles() {
+    // Chargement du style.css du thème parent Twenty Twenty
+    wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
+    // Chargement du css/theme.css pour nos personnalisations
+    wp_enqueue_style('theme-style', get_stylesheet_directory_uri() . '/style.css', array(), filemtime(get_stylesheet_directory() . '/style.css'));
+	
+	// Supprimer les balises p rajoutées par Contact Form7
+	add_filter('wpcf7_autop_or_not', '__return_false');
 }
 
-add_action( 'wp_enqueue_scripts', 'child_enqueue_styles', 15 );
+
+//Si l'utilisateur se connecte
+    function check_login( $user_login, $user ) {
+        
+		
+		echo "<script type='text/javascript'>alert('test');</script>";
+        
+    }
+    add_action('wp_login', 'check_login', 10, 2);
+
+
+//Si l'utilisateur se déconnecte
+    function unlog( $user_login, $user ) {
+        
+        
+    }
+    add_action('wp_logout', 'unlog', 10, 2);
+
+
+?>
